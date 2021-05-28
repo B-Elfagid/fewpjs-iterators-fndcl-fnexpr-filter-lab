992fe48987b6bc7fb30eeb0d26cf67e15a16eb92
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-05-28 12:05:25 +0100

    commit tree 1ceeecafc1514fc655912c8f42e5a13d1cad17dc

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+function matchName(driverList, driverName) {
+    let matchingDriverName = driverList.filter(driver => driver.name === driverName)
+    return matchingDriverName
+}
+
+function findMatching(driverList, driverName) {
+    return driverList.filter(driver => driver.toLowerCase() === driverName.toLowerCase())
+}
+
+function fuzzyMatch(driverList, chars) {
+    return driverList.filter(driver => driver.indexOf(chars) === chars.indexOf(chars))
+}
